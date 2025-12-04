ead9cab9c32ba5f02fe7bc8f3391ccc0fa0dceba
alifzaidan <zaidanalif22710@gmail.com>
2025-12-04 08:21:20 +0700

    update : build

---
+++ public/build/assets/input-error-CWN4Ol1N.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-JjwF2R1D.js";import{c as o}from"./index-DR9RHrKp.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
