fb902abeb3d116f760039284894d4b0d3647850c
alifzaidan <zaidanalif22710@gmail.com>
2025-12-09 13:17:30 +0700

    update : build

--- public/build/assets/input-error-FGsm32XO.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-jPyyk_Hq.js";import{c as o}from"./index-BYsRPGHI.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
